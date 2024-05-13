a2eb3e14a7b21ffef8d0d7efeba7b432eca2a402
Gregory Chapron <gchapron@gmail.com>
2024-05-13 15:01:07 +0200

    virtual environment setup

--- ui_test.py
+++ ui_test.py
@@ -11,19 +11,20 @@ import io
 st.markdown("""# how are you dong, today?
 ## I hope you're doing well...
 Can you post a picture reflecting your current emotion?""")
-#picture = st.camera_input('Take a picture')
-picture = st.file_uploader('Take a picture')
+picture = st.camera_input('Take a picture')
+# picture = st.file_uploader('Take a picture')
 
-#url = 'https://hayd1621-2gsmvh4vlq-ew.a.run.app'
+# url = 'https://hayd1621-2gsmvh4vlq-ew.a.run.app'
 url = 'http://127.0.0.1:8000/upload_your_nice_face'
 
 if picture is not None:
-    
+
     # The key 'img' is the name of the form field for the file upload
     files = {'img': picture}
     # Send the POST request with the image file
     response = requests.post(url, files=files)
     # Resize the image to (224, 224)
     #img = img.resize((224, 224))
+    print('* * * picture taken * * *')
     print(response.reason)
     print(response.content)
