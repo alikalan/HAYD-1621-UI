a66a7865c43c759d09433035ef036c65980a3e1f
Dominik Lambert <dominik.lambert@web.de>
2024-05-14 12:29:45 +0200

    new .py files and sidebar implemented

---
+++ mood_board.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+
+    st.write(f'## Store all the moods from images')
---
+++ project.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+
+    st.write(f'## How is your project going?')
