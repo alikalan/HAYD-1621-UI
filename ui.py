40cd6a8c3cb268fa4504954a42209b8a6ea85886
Ali Kalantarnia <ali.kalantarnia@mail.de>
2024-05-24 12:31:25 +0200

    changed container url

--- ui.py
+++ ui.py
@@ -86,7 +86,7 @@ def app():
     picture_camera = tab1.camera_input("Please take a picture 🤳", on_change=change_picture_state)
 
 
-    url = 'https://hayd1621-v3-lempkfijgq-ew.a.run.app//upload_your_nice_face'
+    url = 'https://hayd1621-docker-v3-lempkfijgq-ew.a.run.app//upload_your_nice_face'
     # url = 'http://127.0.0.1:8000/upload_your_nice_face'
 
     if picture_upload is not None:
